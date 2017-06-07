ee74f615df72627e5d887e66b295157950e3e003
Ayda Sulca <suni_yuri_94@hotmail.com>
2017-06-07 14:17:55 -0500

    avance-4

--- js/components/stationDetails.js
+++ js/components/stationDetails.js
@@ -6,9 +6,12 @@ const stationDetails =(station) => {
   const col = $('<div class="col-xs-12">');
   const name = $('<h4 class="col-xs-12">'+state.selectedStation.name+'</h4>');
   const address = $('<p class="col-xs-12">'+state.selectedStation.address+'</p>');
+  const products = $('<p class="col-xs-12">'+state.selectedStation.products+'</p>');
+
 
   col.append(name);
   col.append(address);
+  col.append(products);
   row.append(col);
   container.append(row);
 
